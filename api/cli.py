f963a1b1ab830d6c35d353ce477b6cb69d1c04b9
Kukhyeon Heo <sainthkh@gmail.com>
2022-06-01 12:52:33 +0900

    Update problematic companies.

--- api/cli.py
+++ api/cli.py
@@ -28,7 +28,7 @@ def update(args):
     print(openapi.get_total_data('005930', 2020, 4, 24))
 
 def update_companies(args):
-    print('Creating table...')
+    print('테이블 생성 중...')
     cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='companies' ''')
 
     if cur.fetchone()[0] == 1: 
@@ -41,16 +41,30 @@ def update_companies(args):
     cur.execute('''CREATE TABLE companies
         (code text, name text, category text, product text, type integer)''')
 
-    print('Downloading...')
+    print('회사 목록 다운로드 중...')
     c = companies.get_companies()
 
-    print('Inserting data...')
+    print('데이터 입력 중...')
     for index, row in c.iterrows():
         cur.execute('''INSERT INTO companies VALUES (?,?,?,?,?)''', (row['code'], row['code_name'], row['category'], row['product'], 0))
     
+    print('불성실공시법인 목록 다운로드 중...')
+    ci = companies.get_companies_insincerity()
+
+    print('불성실공시법인 데이터 입력 중...')
+    for index, row in ci.iterrows():
+        cur.execute('''UPDATE companies SET type = 1 WHERE code = ?''', (row['code'],))
+
+    print('관리종목 목록 다운로드 중...')
+    cm = companies.get_companies_managing()
+
+    print('관리종목 데이터 입력 중...')
+    for index, row in cm.iterrows():
+        cur.execute('''UPDATE companies SET type = 2 WHERE code = ?''', (row['code'],))
+
     con.commit()
 
-    print('Update finished.')
+    print('업데이트 완료.')
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
