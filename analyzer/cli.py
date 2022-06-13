efc0c714fe59df43422155990ace43d8b2a74455
Kukhyeon Heo <sainthkh@gmail.com>
2022-06-14 08:51:23 +0900

    Add volume-inc command

--- analyzer/cli.py
+++ analyzer/cli.py
@@ -23,15 +23,13 @@ def print_companies(companies):
         for c in companies:
             print('{}({})'.format(c[1], c[0]))
 
-def find(args):
+def volume_diff(args):
     cur.execute('SELECT code, name FROM companies WHERE type = 0')
 
     companies = [[], [], []]
 
     for row in cur.fetchall():
         code, name = row
-
-        # print('검색중: {}({})'.format(name, code))
 
         if table_exists(code):
             cur.execute('''SELECT volume, open, close from '{}' ORDER BY date DESC LIMIT 3'''.format(code))
@@ -62,11 +60,47 @@ def find(args):
     for c in companies:
         print_companies(c)
 
+def volume_and_increase(args):
+    cur.execute('SELECT code, name FROM companies WHERE type = 0')
+
+    companies = [[], [], []]
+
+    for row in cur.fetchall():
+        code, name = row
+
+        if table_exists(code):
+            cur.execute('''SELECT volume, close from '{}' ORDER BY date DESC LIMIT 2'''.format(code))
+
+            data = []
+
+            for d in cur.fetchall():
+                data.append({
+                    'volume': d[0],
+                    'close': d[1],
+                })
+            
+            if len(data) == 0:
+                continue
+
+            if (data[0]['close'] > 1.2 * data[1]['close']): # 20% 이상 상승
+                if data[0]['volume'] > 10000000:
+                    companies[0].append((code, name))
+                elif data[0]['volume'] > 5000000:
+                    companies[1].append((code, name))
+                else:
+                    companies[2].append((code, name))
+    
+    for c in companies:
+        print_companies(c)
+
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
-parser_sub = subparsers.add_parser('find')
-parser_sub.set_defaults(func=find)
+parser_sub = subparsers.add_parser('volume-diff')
+parser_sub.set_defaults(func=volume_diff)
+
+parser_sub = subparsers.add_parser('volume-inc')
+parser_sub.set_defaults(func=volume_and_increase)
 
 args = parser.parse_args()
 args.func(args)
